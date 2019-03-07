31f05018ec3e4038f0a9503c6870e1648f8466b1
Bachmann <bernhard.bachmann@fh-bielefeld.de>
2019-03-07 10:28:24 +0100

    Version 0.4

--- FiliP/Config/starten.cmd
+++ FiliP/Config/starten.cmd
@@ -1,2 +1,2 @@
-SET path=%path%D:\OpenModelica1.13.0-dev-64bit\bin
-start /wait D:\FiliP-master\FiliP-master\FiliP\FiliP-Excel.xlsm
+SET path=%path%D:\OpenModelica1.12.0-64bit\bin
+start /wait V:\FILIP\FiliP-master\FiliP\FiliP-Excel.xlsm
